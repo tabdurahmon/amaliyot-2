137d6dc4fd30c0d3ab40c765ed5284d20581697a
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-06-07 10:27:58 +0500

    add

--- js/app.js
+++ js/app.js
@@ -1,5 +1,44 @@
-const form = document.getElementById('form')
-const formButton = document.getElementById('form__button')
-const user = document.getElementById('user')
-const deleteBtn = document.getElementById('delete__btn')
-const clearBtn = document.getElementById('clear__button')
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  const input = document.getElementById("form__input");
+  const userList = document.getElementById("user");
+  const refreshButton = document.getElementById("form__button");
+  const clearButton = document.getElementById("clear__button");
+
+  input.addEventListener("input", (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const users = document.querySelectorAll(".user__item");
+
+    users.forEach((user) => {
+      const name = user
+        .querySelector(".user__name span:nth-child(2)")
+        .textContent.toLowerCase();
+      if (name.includes(searchTerm)) {
+        user.style.display = "";
+      } else {
+        user.style.display = "none";
+      }
+    });
+  });
+
+  refreshButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    updateUserList();
+  });
+
+  clearButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    input.value = "";
+    const users = document.querySelectorAll(".user__item");
+    users.forEach((user) => {
+      user.style.display = "";
+    });
+  });
+
+  userList.addEventListener("click", (event) => {
+    if (event.target.closest(".user__delete--btn")) {
+      const userItem = event.target.closest(".user__item");
+      userList.removeChild(userItem);
+    }
+  });
+});
--- js/request.js
+++ js/request.js
@@ -1,38 +1,86 @@
-// api
-const API = "https://randomuser.me/api/?results=9";
+// // api
+// const API = "https://randomuser.me/api/?results=9";
 
-// for leader
+// // for leader
 
-const overlay = document.getElementById("overlay");
+// const overlay = document.getElementById("overlay");
 
-const loaderToggle = () => {
-  if (toggle) {
-    overlay.classList.remove("hidden");
-  } else {
-    overlay.classList.add("hidden");
-  }
-};
+// const loaderToggle = () => {
+//   if (toggle) {
+//     overlay.classList.remove("hidden");
+//   } else {
+//     overlay.classList.add("hidden");
+//   }
+// };
 
-const getData = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// const getData = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState < 4) {
-        loaderToggle(true);
-      } else if (request.readyState < 4 && request.status == 200) {
-        const data = JSON.parse(request.responseText);
-        console.log(data.results);
-        loaderToggle(false);
-      } else if (request.request == 4) {
-        console.log("Error !!!");
-        loaderToggle(false);
-      }
-    });
+//     request.addEventListener("readystatechange", () => {
+//       if (request.readyState < 4) {
+//         loaderToggle(true);
+//       } else if (request.readyState < 4 && request.status == 200) {
+//         const data = JSON.parse(request.responseText);
+//         console.log(data.results);
+//         loaderToggle(false);
+//       } else if (request.request == 4) {
+//         console.log("Error !!!");
+//         loaderToggle(false);
+//       }
+//     });
 
-    request.open("Get", resource);
-    request.send();
+//     request.open("Get", resource);
+//     request.send();
+//   });
+// };
+
+// getData(API);
+// ///////
+async function fetchUsers() {
+  const API = await fetch("https://randomuser.me/api/?results=9");
+  const data = await API.json();
+  return data.results;
+}
+
+async function updateUserList() {
+  const userList = document.getElementById("user");
+  userList.innerHTML = "";
+  const users = await fetchUsers();
+
+  users.forEach((user) => {
+    const userItem = document.createElement("li");
+    userItem.className = "user__item";
+    userItem.innerHTML = `
+        <button class="user__delete--btn">
+          <i class="fas fa-trash"></i>
+        </button>
+        <img class="user__img" alt="User photo" src="${
+          user.picture.large
+        }" width="100" height="100" />
+        <div class="user__name">
+          <span class="material-symbols-outlined">badge</span>
+          <span>- ${user.name.title} ${user.name.first} ${user.name.last}</span>
+        </div>
+        <div class="user__year">
+          <span class="material-symbols-outlined">cake</span>
+          <span>- ${user.dob.age} years old.</span>
+        </div>
+        <div class="user__location">
+          <span class="material-symbols-outlined">person_pin_circle</span>
+          <span>- ${user.location.city}, ${user.location.country}</span>
+        </div>
+        <div class="user__gender">
+          <span class="material-symbols-outlined">${
+            user.gender === "male" ? "man" : "woman"
+          }</span>
+          <span>- ${user.gender}</span>
+        </div>
+      `;
+    userList.appendChild(userItem);
   });
-};
+}
 
-getData(API);
+document.addEventListener("DOMContentLoaded", () => {
+  updateUserList();
+});
